1d4b0923bd2e9df70f2694834b19a15db2fde3cf
Ian Dunn <padawian@gmail.com>
2026-04-21 13:29:36 -0500

    Enhance transform_data tests with genre_ids
    Updated test cases to include genre_ids and handle missing release_date.

--- tests/test_transform_data.py
+++ tests/test_transform_data.py
@@ -3,14 +3,16 @@ from etl import transform_data
 def test_transform_valid_data():
     test_json = {'results':
                     [{'title': "Test Movie",
-                     'popularity': 100.0,
-                     'vote_average': 5.0,
-                     'release_date': "1999-12-31"}]}
+                        'genre_ids': [1],
+                        'popularity': 100.0,
+                        'vote_average': 5.0,
+                        'release_date': "1999-12-31"}]}
     
     res = transform_data(test_json, "2000-01-01")
 
     assert len(res) == 1
     assert res[0]['title'] == "Test Movie"
+    assert res[0]['genre_ids'] == [1]
     assert res[0]['popularity'] == 100.0
     assert res[0]['vote_average'] == 5.0
     assert res[0]['release_date'] == "1999-12-31"
