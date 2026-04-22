c67b93cb755a9cbb55fb73b72582e191fa9b4740
Ian Dunn <padawian@gmail.com>
2026-04-22 17:42:59 -0500

    Modify tests to include 'id' and handle missing fields
    Updated test cases to include 'id' field and handle missing data correctly.

--- tests/test_transform_data.py
+++ tests/test_transform_data.py
@@ -2,15 +2,17 @@ from etl import transform_data
 
 def test_transform_valid_data():
     test_json = {'results':
-                    [{'title': "Test Movie",
-                        'genre_ids': [1],
-                        'popularity': 100.0,
-                        'vote_average': 5.0,
-                        'release_date': "1999-12-31"}]}
+                    [{'id': 1,
+                    'title': "Test Movie",
+                    'genre_ids': [1],
+                    'popularity': 100.0,
+                    'vote_average': 5.0,
+                    'release_date': "1999-12-31"}]}
     
     res = transform_data(test_json, "2000-01-01")
 
     assert len(res) == 1
+    assert res[0]['id'] == 1
     assert res[0]['title'] == "Test Movie"
     assert res[0]['genre_ids'] == [1]
     assert res[0]['popularity'] == 100.0
@@ -19,11 +21,13 @@ def test_transform_valid_data():
 
 def test_transform_missing_data():
     test_json = {'results':
-                    [{'title': "Test Movie",
-                     'popularity': 100.0,
-                     'vote_average': 5.0}]}
+                    [{'id': 1,
+                    'title': "Test Movie",
+                    'popularity': 100.0,
+                    'vote_average': 5.0}]}
     
     res = transform_data(test_json, "2000-01-01")
 
-    assert res[0]['title'] == "Test Movie"
-    assert res[0]['release_date'] == "N/A"
+    assert res[0]['id'] == 1
+    assert res[0]['genre_ids'] == None
+    assert res[0]['release_date'] == None
